2fcf62b8167d07da75f8fcafc211670a04cb33b2
Mahalingam97 <91451945+Mahalingam97@users.noreply.github.com>
2021-11-17 23:59:50 +0530

    Update app.ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,7 +2,10 @@ import express, {Application} from "express";
 var cors = require('cors');
 const app:Application = express();
 const PORT:number = 5000;
-app.use(cors())
+let corsOptions = {
+  origin: '*' // Sensitive
+};
+app.use(cors(corsOptions))
 app.use('/', require('./routes/search'));
 app.listen(PORT,()=>{
     console.log(`Server is listening at port : ${PORT}`);
