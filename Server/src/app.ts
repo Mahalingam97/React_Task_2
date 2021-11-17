9cb589fdcf568b27f6e0d1885e6c5e64783343dc
Mahalingam97 <91451945+Mahalingam97@users.noreply.github.com>
2021-11-17 23:34:05 +0530

    Update app.ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,7 +1,7 @@
 import express, {Application} from "express";
 var cors = require('cors');
 const app:Application = express();
-const PORT:Number = 5000;
+const PORT:number = 5000;
 app.use(cors())
 app.use('/', require('./routes/search'));
 app.listen(PORT,()=>{
